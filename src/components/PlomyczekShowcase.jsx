57f39d041f2188b04c0f309e456f57b457b07309
Paprykxdd <you@example.com>
2025-11-29 22:31:38 +0100

    zmiana tekstu na stronie glowej

--- src/components/PlomyczekShowcase.jsx
+++ src/components/PlomyczekShowcase.jsx
@@ -24,25 +24,7 @@ function PlomyczekShowcase() {
             <p>
                 Dzięki niemu dzieci uczą się, że każdy jest inny i każdy jest wyjątkowy!
             </p>
-            <h2>Pomysł na wizualne cytaty na stronie</h2>
-            <p>
-                Obok głównego tekstu lub w formie małych, kolorowych banerów/chmurek można umieścić
-                cytaty:
-            </p>
-            <ul>
-                <li>„Inny nie znaczy gorszy!”– np. w chmurce nad Płomyczkiem, przy jego ilustracji.</li>
-                <li>„Jesteś wyjątkowy!” – np. w formie rozświetlonego napisu na tle krajobrazu krainy.</li>
-            </ul>
-            <h2>
-                Dodatkowe elementy wizualne:
-            </h2>
-            <ul>
-                <li>Ilustracja Płomyczka w centrum bloku, najlepiej w otoczeniu zielonych łąk i skał.</li>
-                <li>Małe ikony lub grafiki przy cytatach (np. gwiazdki, małe płomyki), które podkreślą
-                    bajkowy klimat.</li>
-                <li>Opcjonalnie animacja chmurki z cytatem pojawiająca się przy przewijaniu strony,
-                    żeby przyciągnąć uwagę dzieci.</li>
-            </ul>
+
           </div>
 
           <div className="showcase-actions">
